7a754470d593fe8a6af41a52674c368690b1ead4
GauravDS <gaurav.sharma@punchh.com>
2016-12-01 14:29:02 +0530

    Full code

--- animation_GDS.py
+++ animation_GDS.py
@@ -1,21 +1,3 @@
-# PyColorAsciiAnimation
-Terminal Color ASCII Animation Program (Python2.7)
-
-### Video
-Awesome library available by asciimatics and [dummy code](http://stackoverflow.com/a/37051472/1084917)
-
-[![rainbow-colored text among ascii noise][1]][2]
-[1]: https://asciinema.org/a/06sabwtc1bw5wfiq23a71ccxq.png
-  [2]: https://asciinema.org/a/06sabwtc1bw5wfiq23a71ccxq?autoplay=1
-
-### Dependency
-  asciimatics (Python2.7)
-
-  `$ sudo pip install asciimatics`
-
-### Code
-`$ python animation_GDS.py`
-```python
 #!/usr/bin/env python
 from asciimatics.effects import RandomNoise  # $ pip install asciimatics
 from asciimatics.renderers import SpeechBubble, Rainbow
@@ -35,4 +17,3 @@ while True:
         break
     except ResizeScreenError:
         pass
-```
